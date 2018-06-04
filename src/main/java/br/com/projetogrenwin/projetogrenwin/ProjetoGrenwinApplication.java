adecfb4cd1d60bfaa36ebee941b9aabc57453489
eduarda.silva <eduarda.silva@univille.br>
2018-06-04 20:06:45 -0300

    first commit

---
+++ src/main/java/br/com/projetogrenwin/projetogrenwin/ProjetoGrenwinApplication.java
@@ -0,0 +1,12 @@
+package br.com.projetogrenwin.projetogrenwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGrenwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGrenwinApplication.class, args);
+	}
+}
